1b9bb353dde7b61a586e8988cd1d8b69bb51f997
Руслан Шхалиев <morehome@mail.ru>
2019-09-18 06:48:57 +0300

    release v1

--- src/components/atoms/VInputCode.vue
+++ src/components/atoms/VInputCode.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="v-input-code-wrap"
-       :class="{valid: isValid, 'no-valid': noValid}"
+  <div
+    class="v-input-code-wrap"
+    :class="{ valid: isValid, 'no-valid': noValid }"
   >
     <input
       class="v-input-code"
@@ -8,54 +9,54 @@
       maxlength="1"
       v-model="codeNum"
       v-focus
-    >
+    />
   </div>
 </template>
 <script>
-  export default {
-    props: {
-      isValid: Boolean,
-      noValid: Boolean
-    },
-    data() {
-      return {
-        codeNum: ''
-      }
-    }
+export default {
+  props: {
+    isValid: Boolean,
+    noValid: Boolean
+  },
+  data() {
+    return {
+      codeNum: ""
+    };
   }
+};
 </script>
 <style lang="scss">
-  .v-input-code-wrap {
-    position: relative;
-    width: 95px;
-    height: 84px;
-    &:not(:last-child) {
-      margin-right: 40px;
-    }
-    &:before {
-      content: '';
-      position: absolute;
-      bottom: -6px;
-      left: 0;
-      display: block;
-      width: 100%;
-      height: 6px;
-      background: #777;
-    }
-    &.valid:before {
-      background: #ffb600;
-    }
-    &.no-valid:before {
-      background: #ff0000;
-    }
+.v-input-code-wrap {
+  position: relative;
+  width: 95px;
+  height: 84px;
+  &:not(:last-child) {
+    margin-right: 40px;
   }
-  .v-input-code {
+  &:before {
+    content: "";
+    position: absolute;
+    bottom: -6px;
+    left: 0;
+    display: block;
     width: 100%;
-    height: 100%;
-    font-size: 60px;
-    color: #000;
-    background: none;
-    border: none;
-    text-align: center;
+    height: 6px;
+    background: #777;
   }
+  &.valid:before {
+    background: #ffb600;
+  }
+  &.no-valid:before {
+    background: #ff0000;
+  }
+}
+.v-input-code {
+  width: 100%;
+  height: 100%;
+  font-size: 60px;
+  color: #000;
+  background: none;
+  border: none;
+  text-align: center;
+}
 </style>
